8e0f7d5fd60e78fe454565a3d77291bf87c9b3f2
Youri Schuurmans <yourish@live.nl>
2017-01-06 22:20:14 +0100

    Added a simple encryption tool

---
+++ StrongTwoWayEncryption/StrongTwoWayEncryption/Encrypter.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace StrongTwoWayEncryption
+{
+    partial class SimpleEncryptor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SimpleEncryptor));
+            this.tbInput = new System.Windows.Forms.TextBox();
+            this.lblInput = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.tbOuput = new System.Windows.Forms.TextBox();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.lblPasswordCheck = new System.Windows.Forms.Label();
+            this.tbPasswordCheck = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tbInput
+            // 
+            this.tbInput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbInput.Location = new System.Drawing.Point(12, 31);
+            this.tbInput.Multiline = true;
+            this.tbInput.Name = "tbInput";
+            this.tbInput.Size = new System.Drawing.Size(409, 36);
+            this.tbInput.TabIndex = 0;
+            // 
+            // lblInput
+            // 
+            this.lblInput.AutoSize = true;
+            this.lblInput.Location = new System.Drawing.Point(13, 13);
+            this.lblInput.Name = "lblInput";
+            this.lblInput.Size = new System.Drawing.Size(31, 13);
+            this.lblInput.TabIndex = 1;
+            this.lblInput.Text = "Input";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(13, 70);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(53, 13);
+            this.lblPassword.TabIndex = 3;
+            this.lblPassword.Text = "Password";
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbPassword.Location = new System.Drawing.Point(12, 88);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(409, 20);
+            this.tbPassword.TabIndex = 2;
+            this.tbPassword.UseSystemPasswordChar = true;
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(13, 172);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(39, 13);
+            this.lblOutput.TabIndex = 5;
+            this.lblOutput.Text = "Output";
+            // 
+            // tbOuput
+            // 
+            this.tbOuput.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbOuput.Location = new System.Drawing.Point(12, 188);
+            this.tbOuput.Multiline = true;
+            this.tbOuput.Name = "tbOuput";
+            this.tbOuput.Size = new System.Drawing.Size(409, 91);
+            this.tbOuput.TabIndex = 4;
+            this.tbOuput.UseSystemPasswordChar = true;
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnEncrypt.Location = new System.Drawing.Point(263, 159);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnEncrypt.TabIndex = 6;
+            this.btnEncrypt.Text = "Encrypt";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDecrypt.Location = new System.Drawing.Point(347, 159);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnDecrypt.TabIndex = 7;
+            this.btnDecrypt.Text = "Decrypt";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // lblPasswordCheck
+            // 
+            this.lblPasswordCheck.AutoSize = true;
+            this.lblPasswordCheck.Location = new System.Drawing.Point(13, 111);
+            this.lblPasswordCheck.Name = "lblPasswordCheck";
+            this.lblPasswordCheck.Size = new System.Drawing.Size(87, 13);
+            this.lblPasswordCheck.TabIndex = 9;
+            this.lblPasswordCheck.Text = "Check Password";
+            // 
+            // tbPasswordCheck
+            // 
+            this.tbPasswordCheck.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbPasswordCheck.Location = new System.Drawing.Point(12, 129);
+            this.tbPasswordCheck.Name = "tbPasswordCheck";
+            this.tbPasswordCheck.Size = new System.Drawing.Size(409, 20);
+            this.tbPasswordCheck.TabIndex = 3;
+            this.tbPasswordCheck.UseSystemPasswordChar = true;
+            // 
+            // SimpleEncryptor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 291);
+            this.Controls.Add(this.lblPasswordCheck);
+            this.Controls.Add(this.tbPasswordCheck);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.tbOuput);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.lblInput);
+            this.Controls.Add(this.tbInput);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(1280, 720);
+            this.MinimumSize = new System.Drawing.Size(234, 276);
+            this.Name = "SimpleEncryptor";
+            this.Text = "Simple Encryptor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbInput;
+        private System.Windows.Forms.Label lblInput;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.TextBox tbOuput;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.Label lblPasswordCheck;
+        private System.Windows.Forms.TextBox tbPasswordCheck;
+    }
+}
+
---
+++ StrongTwoWayEncryption/StrongTwoWayEncryption/Encrypter.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StrongTwoWayEncryption
+{
+    public partial class SimpleEncryptor : Form
+    {
+        public SimpleEncryptor()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            if (tbPassword.Text == tbPasswordCheck.Text)
+            {
+                disableButtons();
+                tbOuput.Text = TwoWayAES.SimpleEncryptWithPassword(tbInput.Text, tbPassword.Text);
+                enableButtons();
+            }
+            else
+            {
+                MessageBox.Show("Passwords don't match!");
+            }
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            if (tbPassword.Text == tbPasswordCheck.Text)
+            {
+                disableButtons();
+                tbOuput.Text = TwoWayAES.SimpleDecryptWithPassword(tbInput.Text, tbPassword.Text);
+                enableButtons();
+            }
+            else
+            {
+                MessageBox.Show("Passwords don't match!");
+            }
+        }
+
+        private void disableButtons()
+        {
+            btnDecrypt.Enabled = false;
+            btnEncrypt.Enabled = false;
+        }
+
+        private void enableButtons()
+        {
+            btnDecrypt.Enabled = true;
+            btnEncrypt.Enabled = true;
+        }
+    }
+}
